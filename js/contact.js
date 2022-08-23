1b70dfb306710f08de39a610a4526d8f3c86bc31
PapaZzi <510709813@qq.com>
2022-08-23 14:25:29 +0800

    as

---
+++ js/contact.js
@@ -0,0 +1,49 @@
+﻿function GEEKFORGEEKS()                                    
+{ 
+    var name = document.forms["RegForm"]["Name"];               
+    var email = document.forms["RegForm"]["EMail"];    
+    var phone = document.forms["RegForm"]["Telephone"];  
+    var what =  document.forms["RegForm"]["Subject"];
+    var msg =  document.forms["RegForm"]["Msg"];  
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    } 
+   
+   if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+
+   
+     if (phone.value == "")                           
+    { 
+        window.alert("Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+   if (what.value == "")                           
+    { 
+        window.alert("Please enter the services you want to know."); 
+        what.focus(); 
+        return false; 
+    } 
+	if (msg.value == "")                           
+    { 
+        window.alert("Please leave your message."); 
+        msg.focus(); 
+        return false; 
+    } 
+
+			
+   window.alert("You have sent form to our professionals successfully!");
+   window.location.reload();
+    return true; 
+}
